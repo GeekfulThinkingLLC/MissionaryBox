52caee104a23c896be925780f8b90456c31188b2
Trevor <trevorhoward2@hotmail.com>
2017-07-04 15:19:30 -0600

    Added font awesome helper, added icons to footer

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -2,4 +2,8 @@ module Spree::BaseHelper
   def logo(image_path = Spree::Config[:logo], img_options: {})
     link_to image_tag(image_path, img_options), spree.root_path
   end
+
+  def link_fa_to(icon_name, text, link)
+    link_to content_tag(:i, text, :class => "fa fa-#{icon_name} fa-icon-padding"), link
+  end
 end
