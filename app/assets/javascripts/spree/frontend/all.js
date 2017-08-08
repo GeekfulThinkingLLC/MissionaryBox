69f9b390d890aca626f0c8cc4816ae7f7f409a0b
Trevor <trevorhoward2@hotmail.com>
2017-08-08 00:05:06 -0600

    FIXED THE JAVASCRIPT ERROR YAAASSSSS!

--- app/assets/javascripts/spree/frontend/all.js
+++ app/assets/javascripts/spree/frontend/all.js
@@ -1,8 +1,13 @@
-//= require jquery.validate/jquery.validate.min
-//= require spree
-//= require spree/frontend/checkout
-//= require spree/frontend/product
-//= require spree/frontend/cart
+// This is a manifest file that'll be compiled into including all the files listed below.
+// Add new JavaScript/Coffee code in separate files in this directory and they'll automatically
+// be included in the compiled file accessible from http://example.com/assets/application.js
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// the compiled file.
+//
+//= require jquery
+//= require jquery_ujs
+//= require spree/frontend
+//= require_tree .
 
 $(document).ready(function() {
   return $('#phone').each(function() {
