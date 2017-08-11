881fc545b09e93791563489412c7c5cf8bc21710
Trevor <trevorhoward2@hotmail.com>
2017-08-10 23:55:45 -0600

    Maybe now!

--- app/overrides/add_email_to_friend_link_to_products.rb
+++ app/overrides/add_email_to_friend_link_to_products.rb
@@ -2,5 +2,6 @@ Deface::Override.new(
   virtual_path: 'spree/products/show',
   name: 'converted_product_description_351026984',
   insert_bottom: '[data-hook="product_description"], #product_description[data-hook]',
-  text: '<div display="none">'
+  text: '',
+  original: ''
 )
