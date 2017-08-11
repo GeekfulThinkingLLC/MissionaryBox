b03322f8ec6fdb052e41f75932f084c057bfe883
Trevor <trevorhoward2@hotmail.com>
2017-08-10 23:47:11 -0600

    try again to fix email a friend for live site

--- app/overrides/add_email_to_friend_link_to_products.rb
+++ app/overrides/add_email_to_friend_link_to_products.rb
@@ -2,5 +2,5 @@ Deface::Override.new(
   virtual_path: 'spree/products/show',
   name: 'converted_product_description_351026984',
   insert_bottom: '[data-hook="product_description"], #product_description[data-hook]',
-  text: ''
+  text: '<div display="none">'
 )
