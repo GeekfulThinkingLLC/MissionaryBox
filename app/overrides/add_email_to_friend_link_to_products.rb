273fe909ce68d87f9bfb323cb14c33dde21df300
Trevor <trevorhoward2@hotmail.com>
2017-08-10 23:41:49 -0600

    Fixed email a friend bug on product show page

--- app/overrides/add_email_to_friend_link_to_products.rb
+++ app/overrides/add_email_to_friend_link_to_products.rb
@@ -2,6 +2,5 @@ Deface::Override.new(
   virtual_path: 'spree/products/show',
   name: 'converted_product_description_351026984',
   insert_bottom: '[data-hook="product_description"], #product_description[data-hook]',
-  text: '<h5 class="email-to-friend">Know someone who would like this? <%= link_to "Email a friend", email_to_friend_path("product", @product) %></h5>',
-  original: '23a81432df67f93cad7563f7b118d6f901b8e42b'
+  text: ''
 )
